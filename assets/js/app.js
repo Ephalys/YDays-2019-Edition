7a1ffe01cc18b5f128952044062fb5b809819cb6
Ephalys <ephalys@live.fr>
2019-04-19 13:36:08 +0200

    add slick partners

--- assets/js/app.js
+++ assets/js/app.js
@@ -43,6 +43,9 @@
                 $('.partners-slider').slick({
                     dots: true,
                     arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
                 });
             },
             smoothScroll: function() {
